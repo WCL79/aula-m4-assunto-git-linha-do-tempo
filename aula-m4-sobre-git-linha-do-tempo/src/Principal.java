6c8800bd0d9ed2224c278d43f382d4d58e6056d6
Weslley Lima <weslley.lima@zup.com.br>
2021-03-01 15:53:04 -0300

    Terceiro commint referente for na classe Principal na branch master

--- aula-m4-sobre-git-linha-do-tempo/src/Principal.java
+++ aula-m4-sobre-git-linha-do-tempo/src/Principal.java
@@ -1,6 +1,12 @@
+import enumerador.Aluno;
+
 public class Principal {
 
     public static void main(String[] args) {
         System.out.println("Esse são os alunos do Catalisa");
+        for(Aluno aluno : Aluno.values()){
+            System.out.println("-> Alunos são:"+aluno);
+        }
+
     }
 }
